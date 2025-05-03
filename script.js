f84829f6230dc5dc67cb3cbe6aa03d1d77cf3267
denisdenx <91784195+denisdenx@users.noreply.github.com>
2025-05-03 09:32:53 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -9,48 +9,42 @@ let canvas2 = document.getElementById("pointsCanvas");
 let ctx2 = canvas2.getContext("2d");
 
 // for drawing the grid
-let rows = 17;
-let columns = 17;
-let cellSize = 40;
+let rows = 17
+let columns = 17
+let cellSize = 40
 
-let direction = 1;
+let direction = 1
+let tempDirection = 1
 
-let tempX = 1;
-let tempY = 1;
+let tempX = 1
+let tempY = 1
 
-let ateApple = 0;
+let ateApple = 0
 
-// the list of all squares on the grid
-let cords = [];
+let lost = 0
 
 // keep track of the snake
-let tail = [];
-
-// to know if the square on the grid has the snake on it
-let used = 0;
+let tail = []
 
 // these are for the apple
 let tempX2 = Math.floor(Math.random() * 17);
 let tempY2 = Math.floor(Math.random() * 17);
 
-let score = 0;
-
-let currentLocation = [];
+let score = 0
 
 document.addEventListener("keydown", function(event) {
-    if (event.key == "w") {
-        direction = 0;
+    if (event.key === "w" || event.key == "W") {
+        direction = 0
     }
-    if (event.key == "d") {
-        direction = 1;
+    if (event.key === "d" || event.key == "D") {
+        direction = 1
     }
-    if (event.key == "s") {
-        direction = 2;
+    if (event.key === "s" || event.key == "S") {
+        direction = 2
     }
-    if (event.key == "a") {
-        direction = 3;
+    if (event.key === "a" || event.key == "A") {
+        direction = 3
     }
-    console.log(direction);
 });
 
 function drawGrid() {
@@ -58,140 +52,163 @@ function drawGrid() {
     ctx.strokeStyle = "black";
     for (let i = 0; i <= rows; i++) {
         for (let j = 0; j <= columns; j++) {
-            let y = j * cellSize;
-            let x = i * cellSize;
+            let y = j * cellSize
+            let x = i * cellSize
             ctx.strokeRect(j * cellSize, i * cellSize, cellSize, cellSize);
-            cords.push({ i, j, x, y, used});
         }
     }
 }
 
-function drawSnake(x, y) {
-    ctx.fillStyle = "green";
-    ctx.fillRect(x, y, cellSize, cellSize)
-}
-
 function drawApple(x, y) {
-    ctx.fillStyle = "red";
+    ctx.fillStyle = "red"
     ctx.fillRect(x, y, cellSize, cellSize)
     ctx.strokeRect(x, y, cellSize, cellSize)
 }
 
 function move() {
-    if (direction == 0) {
-        tempY = tempY - 1
-    }
-    if (direction == 1) {
-        tempX = tempX + 1
-    }
-    if (direction == 2) {
-        tempY = tempY + 1
-    }
-    if (direction == 3) {
-        tempX = tempX - 1
+    if (tempDirection - 2 !== direction && tempDirection + 2 !== direction) {
+        if (direction === 0) {
+            tempY = tempY - 1
+            tempDirection = direction
+        }
+        if (direction === 1) {
+            tempX = tempX + 1
+            tempDirection = direction
+        }
+        if (direction === 2) {
+            tempY = tempY + 1
+            tempDirection = direction
+        }
+        if (direction === 3) {
+            tempX = tempX - 1
+            tempDirection = direction
+        }   
+    } else {
+        direction = tempDirection
+        if (direction === 0) {
+            tempY = tempY - 1
+        }
+        if (direction === 1) {
+            tempX = tempX + 1
+        }
+        if (direction === 2) {
+            tempY = tempY + 1
+        }
+        if (direction === 3) {
+            tempX = tempX - 1
+        }
     }
 }
 
-/*function checkIfUsed() {
-    currentLocation = cords.find(l => l.j === tempY && l.i === tempX);
-
-    currentLocation.used = score + 3
-
-    console.log(currentLocation);
-
-    for (let i = 0; i < cords.length; i++) {
-        if (cords[i].used > 0) {
-          console.log(cords[i]);
-          
-          ctx.fillStyle = "green";
-          ctx.fillRect(cords[i].x, cords[i].y, cellSize, cellSize)
-          ctx.strokeRect(cords[i].x, cords[i].y, cellSize, cellSize)
-        }
-    }
-
-    for (let i = 0; i < cords.length; i++) {
-        if (cords[i].used > 0) {
-            cords[i].used = cords[i].used - 1;
-        }
-    }
-}*/
-
 function checkIfUsed() {
-    for (let i = 0; i < tail.length; i++) {
-        if (tail.includes(tail[i].tempY === tempY && tail[i].tempX === tempX) === true) {
-            console.log("you lost");
-        }
-    }
-    tail.push({tempX, tempY, sc: 0});
-    tail[tail.length-1].sc += score+3;
     if (ateApple === 1) {
         for(let i = 0; i < tail.length; i++) {
-            tail[i].sc = tail[i].sc + 1;
+            tail[i].sc = tail[i].sc + 1
         }
     }
 
     for (let i = 0; i < tail.length; i++) {
         if (tail[i].sc > 0) {
-            tail[i].sc = tail[i].sc - 1;
+            tail[i].sc = tail[i].sc - 1
         }
         if (tail[i].sc === 0) {
             tail.splice(i, 1);
-            i--;
+            i--
         }
     }
 
-    for (let i = 0; i < tail.length; i++) {
-
-        ctx.fillStyle = "green";
-        ctx.fillRect(tail[i].tempX*40, tail[i].tempY*40, cellSize, cellSize)
-        ctx.strokeRect(tail[i].tempX*40, tail[i].tempY*40, cellSize, cellSize)
+    if (tail.some(obj => obj.x === tempX && obj.y === tempY)) {
+        lost = 1
     }
-    console.log(tail);
-}
 
-function findPosition() {
-    let currentLocation = cords.find(l => l.j === tempY && l.i === tempX);
+    tail.push({x: tempX, y: tempY, sc: 0});
+
+    tail[tail.length-1].sc = score+3
+
+    for (let i = 0; i < tail.length; i++) {
+        ctx.fillStyle = "green"
+        ctx.fillRect(tail[i].x*cellSize, tail[i].y*cellSize, cellSize, cellSize)
+        ctx.strokeRect(tail[i].x*cellSize, tail[i].y*cellSize, cellSize, cellSize)
+    }
 }
 
 function showPointsImage() {
-    ctx2.lineWidth = 5; // Set the thickness
-    ctx2.fillStyle = "red";
+    ctx2.lineWidth = 5
+    ctx2.fillStyle = "red"
     ctx2.fillRect(0, 0, 60, 60)
     ctx2.strokeRect(0, 0, 60, 60)
 }
 
-function showPoints(){
-    scoreText.innerHTML = score;
+function showPoints() {
+    scoreText.innerHTML = score
 }
 
-// erases everything and then redraws
-function execute() {
-    ateApple = 0;
+function appleEaten() {
+    if (tempY === tempY2 && tempX === tempX2) {
+        tempX2 = Math.floor(Math.random() * rows);
+        tempY2 = Math.floor(Math.random() * columns);
+        while (tail.some(obj => obj.x === tempX2 && obj.y === tempY2)) {
+            tempX2 = Math.floor(Math.random() * rows);
+            tempY2 = Math.floor(Math.random() * columns);
+        }
+        console.log("ate apple");
+        score = score + 1
+        ateApple = 1
+    }
+}
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //ctx2.clearRect(0, 0, canvas2.width, canvas2.height);
+function checkOutOfBounds() {
+    if (tempX < 0 || tempX >= rows || tempY < 0 || tempY >= columns) {
+        lost = 1
+    }
+    if (lost === 1) {
+        console.log("you lost");
+    }
+}
 
-    drawGrid();
+if (lost === 0) {
+    ateApple = 0
 
     move();
 
-    drawApple(tempX2*40, tempY2*40);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    appleEaten();
+
+    drawGrid();
+
+    drawApple(tempX2*cellSize, tempY2*cellSize);
     
-    if (tempY == tempY2 && tempX == tempX2) {
-        tempX2 = Math.floor(Math.random() * 17);
-        tempY2 = Math.floor(Math.random() * 17);
-        console.log("ate apple");
-        score = score + 1;
-        ateApple = 1;
-    }
     checkIfUsed();
 
     showPoints();
+}
+// erases everything and then redraws
+function execute() {
+    if (lost === 0) {
+        ateApple = 0
+    
+        move();
+    
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+        appleEaten();
+    
+        drawGrid();
+    
+        drawApple(tempX2*cellSize, tempY2*cellSize);
+        
+        checkIfUsed();
 
-    console.log("score: " + score);
+        checkOutOfBounds();
+    
+        showPoints();
+    }
 }
 
 showPointsImage();
 execute();
 
-setInterval(execute ,500)
+
+setInterval(execute ,250)
+
